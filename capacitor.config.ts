03347fd7bba26d4fef5e542d9b6f33618ed5234a
Anand Kt <anandktkumar1212@gmail.com>
2026-01-08 14:33:55 +0000

    fix capacitor webDir to out

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anand.aura',
+  appName: 'Aura',
+  webDir: 'out',
+};
+
+export default config;
